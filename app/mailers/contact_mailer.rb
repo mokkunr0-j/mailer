0ee7a1dbb46e7bf25f8c4fb3e6d0625586999c0c
jun yamada <jacqk.rv@gmail.com>
2020-10-19 12:31:56 +0900

    アドレス変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "jacqk.rv@gmail.com", subject: "お問い合わせの確認メール"
+    mail to: "yokohu.bird@gmail.com", subject: "お問い合わせの確認メール"
   end
 end
