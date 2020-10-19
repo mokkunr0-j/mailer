efde4bab7677d3a65feb7a559ce8307b5cbfa1ac
jun yamada <jacqk.rv@gmail.com>
2020-10-19 11:29:12 +0900

    誤字修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options{host: 'enigmatic-beyond-36115.herokuapp.com'}
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smpt_settiings = {
+  ActionMailer::Base.smpt_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
